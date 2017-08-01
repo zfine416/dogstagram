48b966fead2ff8aa034093330c59a8eb9c8caeb0
Fine <910675@zachary-fine-1682.nyhq.nytint.com>
2017-08-01 11:26:39 -0400

    updated buttons --- WIP

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Buttons extends Component {
     render() {
         return (
-            <div className="buttons">Some Random Filler Text</div>
+            <div className="buttons">
+                <a className="discover" href=""></a>
+                <a className="heart" href=""></a>
+                <a className="profile" href=""></a>
+            </div>
         )
     }
 }
